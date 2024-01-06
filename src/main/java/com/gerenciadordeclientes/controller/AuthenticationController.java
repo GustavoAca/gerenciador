2671e9ab61ac6a796d34e54a19ce0fe4b72b92a4
Gustavo Acácio <gustavo.acacio@attornatus.com.br>
2024-01-05 21:27:35 -0300

    ajustes

--- src/main/java/com/gerenciadordeclientes/controller/AuthenticationController.java
+++ src/main/java/com/gerenciadordeclientes/controller/AuthenticationController.java
@@ -7,7 +7,6 @@ import com.gerenciadordeclientes.domain.user.User;
 import com.gerenciadordeclientes.service.AuthenticationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
